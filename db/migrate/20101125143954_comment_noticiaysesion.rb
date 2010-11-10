a36d196f4a693479f16f90571d2caba67af73156
Cristian Planas <gawyntrakand@gmail.com>
2010-11-10 16:47:32 +0100

    Arreglat problema de la db amb comments2

--- db/migrate/20101125143954_comment_noticiaysesion.rb
+++ db/migrate/20101125143954_comment_noticiaysesion.rb
@@ -1,6 +1,5 @@
 class CommentNoticiaysesion < ActiveRecord::Migration
   def self.up
-    remove_column :comments, :post_id
     add_column :comments, :noticia_id, :integer
     add_column :comments, :sesion_id, :integer
   end
@@ -8,6 +7,5 @@ class CommentNoticiaysesion < ActiveRecord::Migration
   def self.down
     remove_column :comments, :noticia_id
     remove_column :comments, :sesion_id
-    add_column :comments, :post_id, :integer
   end
 end
